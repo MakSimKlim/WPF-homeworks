08e567fd1a16a5d924c545153409a27b3bb8e8e8
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-12-20 22:39:35 +0500

    Add files via upload

---
+++ ДЗ №44 до 18.12.23_WPF_Введение/ДЗ №44 до 18.12.23_WPF_Введение (дз на С# загадано программой).cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork44
+{
+    class NumberGuesser
+    {
+        // Метод для запуска игры
+        public void PlayGame()
+        {
+            do
+            {
+                // Получаем случайное число, которое пользователь должен угадать
+                int targetNumber = GetRandomNumber(1, 2001);
+                // Вызываем метод для угадывания числа
+                int guessCount = GuessNumber(targetNumber);
+
+                // Выводим результат игры
+                Console.WriteLine($"Поздравляю! Вы угадали число {targetNumber} за {guessCount} попыток.");
+
+                // Предлагаем пользователю сыграть еще раз
+                Console.Write("Хотите сыграть еще раз? (да/нет): ");
+                string playAgain = Console.ReadLine().ToLower();
+
+                if (playAgain != "да")
+                    break;
+
+            } while (true);
+        }
+
+        // Метод для угадывания числа
+        private int GuessNumber(int targetNumber)
+        {
+            int attempts = 0;
+
+            // Выводим инструкции пользователю
+            Console.WriteLine($"Загадано число от 1 до 2000.");
+
+            do
+            {
+                // Запрашиваем у пользователя догадку
+                Console.Write("Введите вашу догадку: ");
+                int userGuess;
+                
+                // Проверяем корректность ввода
+                while (!int.TryParse(Console.ReadLine(), out userGuess) || userGuess < 1 || userGuess > 2000)
+                {
+                    Console.WriteLine("Некорректный ввод. Пожалуйста, введите число от 1 до 2000.");
+                    Console.Write("Введите вашу догадку: ");
+                }
+
+                attempts++;
+
+                // очищаем консоль после каждого ввода
+                Console.Clear();
+
+                // Проверяем, угадал ли пользователь число
+                if (userGuess == targetNumber)
+                    break;
+                else
+                    Console.WriteLine("Неправильно. Попробуйте снова.");
+
+            } while (true);
+
+            return attempts;
+        }
+
+        // Метод для получения случайного числа в заданном диапазоне
+        private int GetRandomNumber(int minValue, int maxValue)
+        {
+            Random random = new Random();
+            return random.Next(minValue, maxValue);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Создаем экземпляр класса и запускаем игру
+            NumberGuesser numberGuesser = new NumberGuesser();
+            numberGuesser.PlayGame();
+        }
+    }
+}
---
+++ ДЗ №44 до 18.12.23_WPF_Введение/ДЗ №44 до 18.12.23_WPF_Введение дз (на С# загадано юзером).cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork44
+{
+    class NumberGuesser
+    {
+        // Метод для угадывания числа, загаданного пользователем
+        public void GuessUserNumber()
+        {
+            do
+            {
+                // Получаем от пользователя загаданное число
+                int targetNumber = GetUserTargetNumber();
+
+                // Запускаем процесс угадывания
+                int guessCount = AutoGuess(targetNumber);
+
+                // Выводим результат угадывания
+                Console.WriteLine($"Программа угадала ваше число {targetNumber} за {guessCount} попыток.");
+
+
+                // Предлагаем пользователю сыграть еще раз
+                Console.Write("Хотите сыграть еще раз? (да/нет): ");
+                string playAgain = Console.ReadLine().ToLower();
+
+                // ToLower() используется для преобразования всех символов в строке к нижнему регистру (в нижний регистр).
+                // Это делается для того, чтобы сделать введенный текст нечувствительным к регистру при сравнении
+
+                if (playAgain != "да")
+                    break;
+
+                Console.Clear(); // Очищаем консоль перед новой игрой
+
+            } while (true);
+        }
+
+        // Метод для автоматического угадывания числа
+        private int AutoGuess(int targetNumber)
+        {
+            int min = 1;
+            int max = 2000;
+            // счетчик, который отражает количество шагов, предпринятых программой в процессе угадывания числа
+            int attempts = 0;
+
+            do
+            {
+                //GetMidpoint вычисляет среднее значение между двумя числами (min и max),
+                //чтобы получить "предполагаемое" число, которое программа проверяет
+                //на соответствие загаданному пользователем числу
+                int guess = GetMidpoint(min, max);
+                attempts++;
+
+                // Сравниваем угаданное число с загаданным
+                if (guess == targetNumber)
+                {
+                    break;
+                }
+                else if (guess < targetNumber)
+                {
+                    // Если угаданное число меньше, увеличиваем минимальное значение диапазона
+                    min = guess + 1;
+                }
+                else
+                {
+                    // Если угаданное число больше, уменьшаем максимальное значение диапазона
+                    max = guess - 1;
+                }
+
+            } while (true);
+
+            return attempts;
+        }
+
+        // Метод для получения середины диапазона
+        private int GetMidpoint(int min, int max)
+        {
+            return (min + max) / 2;
+        }
+
+        // Метод для получения числа, которое пользователь загадал
+        private int GetUserTargetNumber()
+        {
+            Console.WriteLine("Загадайте число от 1 до 2000.");
+
+            while (true)
+            {
+                
+                Console.Write("Введите ваше загаданное число: ");
+                int userTarget;
+
+                if (int.TryParse(Console.ReadLine(), out userTarget) && userTarget >= 1 && userTarget <= 2000)
+                {
+                    return userTarget;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод. Пожалуйста, введите число от 1 до 2000.");
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Программа << Угадай число >> ");
+            NumberGuesser numberGuesser = new NumberGuesser();
+            numberGuesser.GuessUserNumber();
+        }
+    }
+
+}
